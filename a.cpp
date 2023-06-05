e164b20ef40fb8e40396515777dbc8acb7db3790
Rahul Panwar <panwarr404@gmail.com>
2023-06-05 20:06:06 +0530

    Files created

---
+++ a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define in(a,n); int a[n]; for(int i=0;i<n;i++){cin>>a[i];}
+#define add(a,b,c) for(int z=0;z<b;z++){c+=a[z];}
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+
+int main()
+{
+    fio;
+    int t;
+    cin>>t;
+    
+    return 0;
+}
