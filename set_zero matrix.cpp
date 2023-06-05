d64a424ad606148219d4ac74c06e396b8b996390
Rahul Panwar <panwarr404@gmail.com>
2023-06-05 20:41:03 +0530

    code

--- set_zero matrix.cpp
+++ set_zero matrix.cpp
@@ -1,16 +1,34 @@
-#include <iostream>
 #include <bits/stdc++.h>
-#define ll long long
-#define in(a,n); int a[n]; for(int i=0;i<n;i++){cin>>a[i];}
-#define add(a,b,c) for(int z=0;z<b;z++){c+=a[z];}
-#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-using namespace std;
 
-int main()
-{
-    fio;
-    int t;
-    cin>>t;
+void setZeros(vector<vector<int>>& matrix) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
     
-    return 0;
+    vector<bool> rowZeros(rows, false);
+    vector<bool> colZeros(cols, false);  
+    
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (matrix[i][j] == 0) {
+                rowZeros[i] = true;
+                colZeros[j] = true;
+            }
+        }
+    }
+    
+    for (int i = 0; i < rows; i++) {
+        if (rowZeros[i] == 1) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    
+    for (int j = 0; j < cols; j++) {
+        if (colZeros[j] == 1) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
 }
